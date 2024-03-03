84e8dfcdc729552e0c18403c40c13bd5ed38ad3e
Hare <harekrishna.bhuit15@gmail.com>
2024-03-03 12:36:05 +0530

    first commit

---
+++ src/main/java/com/hky/common/service/CommonsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hky.common.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommonsServiceApplication.class, args);
+	}
+
+}
